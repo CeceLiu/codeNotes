3008672782bf8711659a2517d977148b8f7596ea
CeceLiu <xliu.cece@gmail.com>
2017-02-06 23:22:01 -0500

    All permutations of the string with no duplicated letters

---
+++ Class06_DFS/permutation.java
@@ -0,0 +1,31 @@
+public class Solution {
+  public List<String> permutations(String set) {
+    // Write your solution here.
+    List<String> solu = new ArrayList<String>();
+    if (set == null)
+      return solu;
+    char[] array = set.toCharArray();
+    permutation(array, 0, solu);
+    return solu;
+  }
+  
+  public void permutation(char[] array, int index, List<String> solu){
+    if (index == array.length){
+      solu.add(new String(array));
+      return;
+    }
+    for(int i=index; i<array.length; i++){
+      swap(array, i, index);
+      permutation(array, index+1, solu);
+      swap(array, i, index);
+    }
+  }
+  
+  public void swap(char[] array, int i, int j){
+    if (i == j)
+      return;
+    char temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
