404ba7fc155ff7fe1b31941107a85bf91311b1a5
CeceLiu <xliu.cece@gmail.com>
2017-02-07 21:05:26 -0500

    Only two states at each level: add the letter OR not

--- Class06_DFS/allSubsets.java
+++ Class06_DFS/allSubsets.java
@@ -12,11 +12,23 @@ public class Solution {
   }
   
   public void subSets(char[] array, int index, StringBuilder sbd, List<String> solu){
+    /*
     solu.add(sbd.toString());
     for(int i=index; i<array.length; i++){
       sbd.append(array[i]);
+      // pass i or index is very different
       subSets(array, i+1, sbd, solu);
       sbd.deleteCharAt(sbd.length()-1);
     }
+    */
+    // only two sinario: add the letter OR not
+    if (index == array.length){
+      solu.add(sbd.toString());
+      return;
+    }
+    sbd.append(array[index]);
+    subSets(array, index+1, sbd, solu);
+    sbd.deleteCharAt(sbd.length()-1);
+    subSets(array, index+1, sbd, solu);
   }
 }
