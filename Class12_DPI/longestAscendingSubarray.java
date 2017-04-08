31d7d3fa399c2e06186b59ee22c4a55e11f7396b
CeceLiu <xliu.cece@gmail.com>
2017-04-08 14:56:20 -0400

    Add comments

--- Class12_DPI/longestAscendingSubarray.java
+++ Class12_DPI/longestAscendingSubarray.java
@@ -3,6 +3,8 @@ public class Solution {
     // Write your solution here.
     if (array == null || array.length <1)
       return 0;
+
+    // use O(1) space for local maxi and compare with global
     int currL = 1;
     int max = currL;
     for(int i=1; i<array.length; i++){
